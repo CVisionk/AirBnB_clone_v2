25766d36adacf9e3744f302eb25b7dc6ab070c2d
Collen <damd819@gmail.com>
2024-06-29 14:37:24 +0200

    FIxeed extra print

--- console.py
+++ console.py
@@ -51,7 +51,6 @@ class HBNBCommand(cmd.Cmd):
 
             kwargs = {}
             for i in range(1, len(my_list)):
-                print(my_list)
                 key, value = tuple(my_list[i].split("="))
                 if value[0] == '"':
                     value = value.strip('"').replace("_", " ")
