8e5042a44370dbd4a07d3840ac05a6876a62fbc2
Collen <damd819@gmail.com>
2024-07-11 13:41:02 +0200

    Added 0-setup...

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Install Nginx if it is not already installed
+if ! dpkg -s nginx > /dev/null 2>&1; then
+    sudo apt update
+    sudo apt install -y nginx
+fi
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+if [ -L /data/web_static/current ]; then
+    sudo rm -f /data/web_static/current
+fi
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of /data/ to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration to serve the content
+config="server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}"
+
+echo "$config" | sudo tee /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
